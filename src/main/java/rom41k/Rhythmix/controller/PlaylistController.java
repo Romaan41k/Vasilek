5975a9521415be78657ca2fce3dd49a5b8255df9
rom41k <ignatovicroma844@gmail.com>
2024-12-11 23:38:11 +0300

    basic functionality

--- src/main/java/rom41k/Rhythmix/controller/PlaylistController.java
+++ src/main/java/rom41k/Rhythmix/controller/PlaylistController.java
@@ -21,6 +21,7 @@ public class PlaylistController {
         return ResponseEntity.ok(createdPlaylist);
     }
 
+
     @GetMapping("/{id}")
     public ResponseEntity<Playlist> getPlaylistById(@PathVariable Long id) {
         return playlistService.findById(id)
